53c5f9ea0fda752a969892abc9773e703cc81198
Jorge Fernandez <jfernandezcolli@gmail.com>
2017-08-19 23:09:02 -0500

    Se agrega la funcion onFBlogin que servira para solicitar los datos del usuario a facebook.

--- js/main.js
+++ js/main.js
@@ -1,1 +1,32 @@
-m
+var myApp= angular.module("myApp",[]);
+
+myApp.controller('myController',['$scope',function($scope){
+
+	$scope.facebook= {
+		username:"",
+		email:""
+	};
+
+	$scope.onFBlogin=function(){
+		FB.login(function(response){
+			if (response.authResponse) {
+				FB.api('/me','GET',{fields:'email, first_name, name, id, picture'},function(response){
+					$scope.$apply(function(){
+						$scope.facebook.username=response.name;
+						$scope.facebook.email=response.email;
+						$scope.fb_image=response.picture.data.url;
+					});
+				});
+
+			}else{
+				//error
+			}
+
+		} ,	{
+			
+			scope:'email,user_likes',
+			return_scopes:true
+		});
+	}
+
+}]);
